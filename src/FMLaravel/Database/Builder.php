70dc5dabeb4500b0482f553da34ee27984af3736
Andries <demobiel@users.noreply.github.com>
2017-04-06 12:55:24 +0200

    Update Builder.php

--- src/FMLaravel/Database/Builder.php
+++ src/FMLaravel/Database/Builder.php
@@ -25,33 +25,6 @@ class Builder extends EloquentBuilder
 
         return $this;
     }
-    <?php namespace FMLaravel\Database;
-
-use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
-use Illuminate\Database\Eloquent\Relations\Relation;
-
-use Closure;
-
-class Builder extends EloquentBuilder
-{
-
-    /**
-     * Set the relationships that should be eager loaded.
-     *
-     * @param  mixed  $relations
-     * @return $this
-     */
-    public function with($relations)
-    {
-        parent::with($relations);
-
-        $eagerLoad = array_keys($this->eagerLoad);
-        $eagerLoad = array_intersect($eagerLoad, array_keys($this->model->getRelatedRecordsInfo()));
-
-        $this->query->setEagerLoad($eagerLoad);
-
-        return $this;
-    }
 
     /**
      * Get the hydrated models without eager loading.
